5bf1d90a35c70c610eafb260b27a54a26f7ebf2c
Timothy Oon <timothyoon@gmail.com>
2011-05-03 22:42:11 +1000

    User model with passwords and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,73 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor   :password
+
+  attr_accessible :name,
+                  :email, 
+                  :password,
+                  :password_confirmation
   
+  
+  ###############################################
   # Validation methods
+  ###############################################
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name,  :presence => true,
                     :length => { :minimum => 3, :maximum => 50 }
+  
   validates :email, :presence => true,
                     :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  
+  validates :password,  :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40}
+  
+  ###############################################
+  # Callbacks
+  ###############################################
+  
+  before_save :encrypt_password
+  
+  ###############################################
+  # Static Methods
+  ###############################################
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return user && user.has_password?(submitted_password) ? user : nil
+  end
+  
+  ###############################################
+  # Public Methods
+  ###############################################
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  ###############################################
+  # Private Methods
+  ###############################################
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+  
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,19 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { :name => "Example User", 
+              :email => "user@example.com",
+              :password => "foobar",
+              :password_confirmation => "foobar" }
   end
   
   it "should create a new instance given valid attributes" do
     User.create!(@attr)
   end
+  
+  ###############################################
+  # Name tests
+  ###############################################
   
   it "should require a name" do
     no_name_user = User.new(@attr.merge(:name => ""))
@@ -31,6 +38,11 @@ describe User do
     short_name_user = User.new(@attr.merge(:name => short_name))
     short_name_user.should_not be_valid
   end
+  
+  
+  ###############################################
+  # Email tests
+  ###############################################
   
   it "should accept valid email addresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.au]
@@ -59,6 +71,88 @@ describe User do
     User.create!(@attr.merge(:email => upper_case_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
+  end
+  
+  
+  ###############################################
+  # Password tests
+  ###############################################
+  
+  describe "password validations" do
+    
+    it "should require a passwoord" do
+      hash = @attr.merge(:password => "", :password_confirmation => "")
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      hash = @attr.merge(:password_confirmation => "invalid")
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+    
+    ###############################################
+    # Password encryption tests
+    ###############################################
+    
+    describe "password encryption" do
+      
+      before(:each) do
+        @user = User.create!(@attr)
+      end
+      
+      it "should have an encrypted password attribute" do
+        @user.should respond_to(:encrypted_password)
+      end
+      
+      it "should set the encrypted password" do
+        @user.encrypted_password.should_not be_blank
+      end
+      
+      describe "has_password? method" do
+        
+        it "should be true if the passwords match" do
+          @user.has_password?(@attr[:password]).should be_true
+        end
+        
+        it "should be false if the passwords don't match" do
+          @user.has_password?("invalid").should be_false
+        end
+        
+      end
+      
+      describe "authenticate method" do
+        
+        it "should return nil on email/password mismatch" do
+          wrong_password_user = User.authenticate(@attr[:email], "wrongpassword")
+          wrong_password_user.should be_nil
+        end
+        
+        it "should return nil when no user exists for an email address" do
+          wrong_password_user = User.authenticate("unknown@user.com", @attr[:password])
+          wrong_password_user.should be_nil
+        end
+        
+        it "should return the user on valid credentials" do
+          wrong_password_user = User.authenticate(@attr[:email], @attr[:password])
+          wrong_password_user.should == @user
+        end
+        
+      end
+      
+    end
+    
   end
   
 end
